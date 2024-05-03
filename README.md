ab1eccd792a9aeaddb06cd89a468b0860d747a71
Deeparasi-arivan <deepa.deeparasi17@gmail.com>
2024-05-03 21:33:39 +0300

    updated

---
+++ README.md
@@ -0,0 +1,2 @@
+
+here we are seeing the installation and folder structure of react  then  today class  notes refer too useful
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import part
+const App = () => {
+  //js part
+  const a = 10;
+  return (
+    //jsx part/html part /css part
+    <>
+      <h1>hello world</h1>
+      {a}
+    </>
+  );
+};
+
+//export part
+export default App;
